719610eed9996fe0f83a21a98cfe3bec8d09773b
Adam <downeyadam0312@gmail.com>
2022-03-06 22:09:56 +0000

    Home screen worked on
    I created the web page for the home screen. This also introduced the
    logo, and background image. The notes webpage has been attempted but
    unknown problems are preventing me from viewing that webpage. It is
    some directory issue I think.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
+  #By using resources we can benefit from using all of the controller's seven actions (index, show, new, etc..)
 	resources :meetings
-	get "home/index"
-	root to: "home#index"
+  get 'home', to: 'home#index'
+  get 'notes', to: 'notes#index'
+  root to: "home#index"
 	# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   	# Defines the root path route ("/")
   	# root "articles#index"
+  #
 end
