271df6af1ecbc01615efd1a3cd430cf43055017c
Adam <downeyadam0312@gmail.com>
2022-04-02 13:27:12 +0100

    Devise and Postgres
    I added the Devise gem and made it functional. I also changed the
    sqllite from before back to psotgres. Hopefully this doesn't break
    things...

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   #By using resources we can benefit from using all of the controller's seven actions (index, show, new, etc..)
 	resources :meetings
-  get 'home', to: 'home#index'
   get 'notes', to: 'notes#index'
-  root to: "home#index"
 	# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   	# Defines the root path route ("/")
   	# root "articles#index"
   #
+  root to: "home#index"
 end
