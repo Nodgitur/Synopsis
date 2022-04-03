8df3a0ad38d8a49aa6c1b262f59f18eca8f8bd57
Adam <downeyadam0312@gmail.com>
2022-04-03 17:27:04 +0100

    Added some error handling for the weather retrieval

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     #Geocoder gem only works in production environment database
     if Rails.env.production?
-      @country = request.location.country
+      @country = request.location.country_code
       @city = request.location.city
     else
       @country = "Location not found"
